a22aa4b593f931604e15f93bb80cebac4ebe8489
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-14 15:15:52 -0400

    Store neeeded classes

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from bs4 import BeautifulSoup  # To use HTML and grab the gathered data
 
 res = requests.get('https://news.ycombinator.com/news')  # Access browser without need of a window
 soup_obj = BeautifulSoup(res.text, 'html.parser')
+# print(soup_obj.find(id='score_36329483'))
+links = soup_obj.select('.titleline')
+votes = soup_obj.select(".score")
