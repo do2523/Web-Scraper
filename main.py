86e3a64fb4344b393f2164ea500e7b6b6b8608ba
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-14 15:33:53 -0400

    Append links and title to array which will be output

--- main.py
+++ main.py
@@ -6,3 +6,16 @@ soup_obj = BeautifulSoup(res.text, 'html.parser')
 # print(soup_obj.find(id='score_36329483'))
 links = soup_obj.select('.titleline')
 votes = soup_obj.select(".score")
+
+def create_custom_hn(links, votes):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText() 
+        href = links[index].get('href', None)  # if no href then it will default to None
+        points = votes[index].getText()
+        print(points)
+        hn.append({'title': title, 'link':href})
+    return hn
+
+create_custom_hn(links,votes)
+# print(create_custom_hn(links, votes))
