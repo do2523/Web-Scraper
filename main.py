2d65790851a43e3367750f07d6cef3e05b002800
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-14 14:41:42 -0400

    Acess external websites HTML

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import requests  # For grabbing HTML files
 from bs4 import BeautifulSoup  # To use HTML and grab the gathered data
+
+res = requests.get('https://news.ycombinator.com/news')  # Access browser without need of a window
+soup_obj = BeautifulSoup(res.text, 'html.parser')
