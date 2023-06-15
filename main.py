a671baa03d9e74e650e36dbbae23b06ed3dd6550
Daniel Ocampo <121493027+do262431@users.noreply.github.com>
2023-06-15 08:14:18 -0400

    Ordered by points and added page 2

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import requests  # For grabbing HTML files
 from bs4 import BeautifulSoup  # To use HTML and grab the gathered data
+import pprint
+
 
 res = requests.get('https://news.ycombinator.com/news')  # Access browser without need of a window
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup_obj = BeautifulSoup(res.text, 'html.parser')
+soup_obj2 = BeautifulSoup(res2.text, 'html.parser')
 # print(soup_obj.find(id='score_36329483'))
-links = soup_obj.select('.titleline')
-votes = soup_obj.select(".score")
+links2 = soup_obj2.select('.titleline')
+subtext2 = soup_obj2.select(".subtext")
 
-def create_custom_hn(links, votes):
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
     hn = []
     for index, item in enumerate(links):
-        title = links[index].getText() 
-        href = links[index].get('href', None)  # if no href then it will default to None
-        points = votes[index].getText()
-        print(points)
-        hn.append({'title': title, 'link':href})
-    return hn
+        title = item.getText() 
+        href = item.get('href', None)  # if no href then it will default to None
+        vote = subtext[index].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href , 'votes': points})
+    return sort_stories_by_votes(hn)
 
-create_custom_hn(links,votes)
+
+pprint.pprint(create_custom_hn(links,subtext))
 # print(create_custom_hn(links, votes))
